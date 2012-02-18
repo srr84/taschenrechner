234d37026df727c8b2236737e9853283eda64d3c
Sven Ruchti <sven.ruchti@gmx.de>
2012-02-18 17:05:12 +0100

    Added environments

---
+++ deploy_qa.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /Users/svenruchti/Dropbox/Master\ Thesis/taschenrechner/environments/dev
+cp Taschenrechner.html /Users/svenruchti/Dropbox/Master\ Thesis/taschenrechner/environments/qa
+cp Taschenrechner.js /Users/svenruchti/Dropbox/Master\ Thesis/taschenrechner/environments/qa
