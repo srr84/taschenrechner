dab612ff01cf722aa874a6776a26ac1edd9e3aec
Sven Ruchti <sven.ruchti@gmx.de>
2012-02-18 20:31:51 +0100

    Changed URL in Gui Test to Environment URL

--- deploy_prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /Users/svenruchti/Dropbox/Master\ Thesis/taschenrechner/environments/qa
-cp Taschenrechner.html /Users/svenruchti/Dropbox/Master\ Thesis/taschenrechner/environments/prod
-cp Taschenrechner.js /Users/svenruchti/Dropbox/Master\ Thesis/taschenrechner/environments/prod
